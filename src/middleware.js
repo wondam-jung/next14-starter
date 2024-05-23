de5d43a1742fba8a4b6ffbeb83ac25575a06e8af
wondam-jung <jungwondam@gmail.com>
2024-05-23 22:04:17 +0900

    git test

--- src/middleware.js
+++ src/middleware.js
@@ -6,7 +6,6 @@ export default NextAuth(authConfig).auth;
 export const config = {
     matcher: ["/((?!api|static|.*\\..*|_next).*)"],
 };
-
 /*
     matcher allows you to filter Middleware to run (or not to run like in the example) on specific paths.
     if you don't add any matchers, Middleware will be invoked for every route in your project.
